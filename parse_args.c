cae3927424bf7feaa5868161c1c865b07ed21a1b
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2023-01-09 19:31:48 +0000

    Add files via upload

--- parse_args.c
+++ parse_args.c
@@ -3,12 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   parse_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 18:56:29 by nuno              #+#    #+#             */
-/*   Updated: 2023/01/04 18:56:52 by nuno             ###   ########.fr       */
+/*   Updated: 2023/01/09 19:27:17 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	ft_atoi(const char *str)
+{
+	int			i;
+	long long	num;
+
+	i = 0;
+	num = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		if (num > 2147483647)
+			return (0);
+		i++;
+	}
+	return (num);
+}
+
+void	parse_args(int ac, char **av)
+{
+	t_data	data;
+
+	data.num_of_philo = ft_atoi(av[1]);
+	data.time_to_die = ft_atoi(av[2]);
+	data.time_to_eat = ft_atoi(av[3]);
+	data.time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		data.num_times_must_eat = ft_atoi(av[5]);
+	return (data);
+}
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 13:24:01 by nuno              #+#    #+#             */
-/*   Updated: 2023/01/03 13:24:58 by nuno             ###   ########.fr       */
+/*   Updated: 2023/01/09 16:58:23 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,16 @@
 # include <sys/time.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_data
+{
+	int	num_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_times_must_eat;
+}				t_data;
+
+void	parse_args(int ac, int av);
 
 #endif
